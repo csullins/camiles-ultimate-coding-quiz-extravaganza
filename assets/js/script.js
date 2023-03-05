2721df5d02fec71a3a9781bd151b61719565627a
csullins <camilesullins@gmail.com>
2023-03-04 19:47:50 -0600

    cleaning up js code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,33 @@
 let container = document.querySelector('.container');
 let header = document.querySelector('#header');
 let cards = document.querySelector('#cards');
-let choices = document.querySelector('#choices');
 let seconds = document.querySelector('.seconds');
 const clickStart = $('.start');
 const quizStart = $('.header');
-
-container.addEventListener('click', function(event) {
-let start = event.target;
-
-if (start.matches('.start')) {
-    header.style.display = 'none';
-    cards.style.display = 'block';
-    question.style.display = 'block';
-    choices.style.display = 'block'
-    }
-});
+let choicesElement = document.querySelector('#choices');
+let questionElement = document.querySelector('#question');
+let currentQuestionIndex = 0;
+let currentScore = 0;
+let timeLeft = 25;
+let timerRunning = false;
 
 clickStart.on('click', function() {
+    header.style.display = 'none';
+    cards.style.display = 'flex';
+    question.style.display = 'flex';
+    choicesElement.style.display = 'flex'
     startTimer();
     displayQuestion(questions[0]);
-})
-
+    });
 
 function startTimer() {
-    var timeLeft = 10;
-  
+    timerRunning = true;
     // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
     var timeInterval = setInterval(function () {
         console.log('Timer has started.');
+    if (!timerRunning) {
+        clearInterval(timeInterval);
+        }
       // As long as the `timeLeft` is greater than 1
       if (timeLeft > 0) {
         seconds.textContent = timeLeft + 's';
@@ -40,78 +39,93 @@ function startTimer() {
         seconds.textContent = '';
         // Use `clearInterval()` to stop the timer
         clearInterval(timeInterval);
+        displayResults();
       }
-
       if (timeLeft < 6) {
         $('.seconds').css({'background-color':'yellow'});
       }
     }, 1000);
   };
 
-  let currentQuestionIndex = 0;
-
-  const questions = [
+const questions = [
       {
-        question: "What is the capital of France?",
-        answers: ["London", "Paris", "Madrid", "Berlin"],
-        correctAnswer: 1 // Index of the correct answer in the answers array
-      },
-      {
-        question: "What is the largest mammal in the world?",
-        answers: ["Elephant", "Giraffe", "Blue whale", "Rhinoceros"],
-        correctAnswer: 2
-      },
-      {
-        question: "who is the cutest labradoodle in the world?",
-        answers: ["Ben", "Remi", "Archie", "Cami"],
-        correctAnswer: 1
+          question: 'What is the capital of France?',
+          answers: ['London', 'Paris', 'Madrid', 'Berlin'],
+          correctAnswer: 1 // Index of the correct answer in the answers array
         },
         {
-        question: "Who is the best husband in the world?",
-        answers: ["Archie", "Remi", "Ben", "Cami"],
-        correctAnswer: 2
+            question: 'What is the largest mammal in the world?',
+            answers: ['Elephant', 'Giraffe', 'Blue whale', 'Rhinoceros'],
+            correctAnswer: 2
+        },
+        {
+            question: 'who is the cutest labradoodle in the world?',
+            answers: ['Ben', 'Remi', 'Archie', 'Cami'],
+            correctAnswer: 1
+        },
+        {
+            question: 'Who is the best husband in the world?',
+            answers: ['Archie', 'Remi', 'Ben', 'Cami'],
+            correctAnswer: 2
         },
     ];
     
-    function displayQuestion(question) {
-        console.log('Questions are displaying.');
-        let questionElement = document.querySelector('#question');
-        let newQuestion = document.createElement('p');
-        newQuestion.textContent = question.question;
-        questionElement.appendChild(newQuestion);
-  
-        let choicesElement = document.querySelector("#choices");
-        question.answers.forEach((answer, index) => {
-        const answerButton = document.createElement("button");
-        answerButton.textContent = answer;
-        answerButton.addEventListener("click", () => {
-          if (index === question.correctAnswer) {
+
+function displayQuestion(question) {
+    console.log('Questions are displaying.');
+
+    let newQuestion = document.createElement('p');
+    newQuestion.textContent = question.question;
+    questionElement.appendChild(newQuestion);
+
+    let answersElement = document.createElement('div');
+    question.answers.forEach((answer, index) => {
+    const answerButton = document.createElement('button');
+
+    answerButton.textContent = answer;
+
+    answerButton.addEventListener('click', () => {
+        
+        if (index === question.correctAnswer) {
             // Display next question if answer is correct
-            currentQuestionIndex++;
-            if (currentQuestionIndex < questions.length) {
-              const nextQuestion = questions[currentQuestionIndex];
-              questionElement.innerHTML = "";
-              displayQuestion(nextQuestion);
-            } else {
-              // End of quiz, display results
-              displayResults();
-            }
-          } else {
+            currentScore++
+        
+        } else {
             // Incorrect answer, display message
-            const messageElement = document.createElement("p");
-            messageElement.textContent = "Incorrect, please try again";
+            const messageElement = document.createElement('p');
+            messageElement.textContent = 'Incorrect';
             choicesElement.appendChild(messageElement);
-          }
-        });
+            timeLeft -= 5;
+        }
+
+        currentQuestionIndex++;
+
+        if (currentQuestionIndex < questions.length) {
+            const nextQuestion = questions[currentQuestionIndex];
+            questionElement.innerHTML = '';
+            displayQuestion(nextQuestion);
+        } else {
+            displayResults();
+        }
+
+    });
         answersElement.appendChild(answerButton);
-      });
-      (choicesElement).appendChild(answersElement);
+    });
+    choicesElement.innerHTML = '';
+    choicesElement.appendChild(answersElement);
     }
-  
+    
+
+
     function displayResults() {
-      const score = questions.filter(question => question.correctAnswer === question.selectedAnswer).length;
-      const resultsElement = document.createElement("h2");
-      resultsElement.textContent = `You scored ${score} out of ${questions.length}`;
-      document.body.innerHTML = "";
-      document.body.appendChild(resultsElement);
+        timerRunning = false;
+        questionElement.innerHTML = '';
+        choicesElement.innerHTML = '';
+        let score = currentScore;
+        const resultsElement = document.querySelector('.card2');
+        resultsElement.innerHTML = `You answered ${score} out of ${questions.length} correctly.`;
+
+        let highScore = document.querySelector('.highScore');
+
+        highScore.innerHTML = timeLeft;
     };
