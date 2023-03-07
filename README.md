e497c9ee0b6235939c5b1ec40ae55771e087ca46
csullins <camilesullins@gmail.com>
2023-03-06 22:40:31 -0600

    added the README and finalize js

---
+++ README.md
@@ -0,0 +1,32 @@
+# Code Quiz Extravanaganza
+This is a simple timed quiz application built using HTML, CSS, and JavaScript.
+
+## Getting Started
+To get started with the quiz application, simply open the index.html file in your web browser. Once you open the file, you will see the home screen with the 'START' button.
+
+## How to Play
+- Click on the 'START' button to begin the quiz.
+The quiz consists of 6 multiple choice questions related to JavaScript, HTML, and CSS.
+- You have 30 seconds to answer all the questions.
+- If you answer a question incorrectly, 5 seconds will be deducted from the timer.
+- If you answer all the questions before the timer runs out, you will see the results of your quiz along with your score.
+- If the timer runs out before you can answer all the questions, the quiz will end and the results will be displayed along with your score.
+- The score corresponds to how much time was left on the clock when you finished the quiz.
+- You will be presented with the option to save your score. - The High Score will only update if the next time beats the previous.
+- File Structure
+
+## The quiz application consists of three main files:
+
+- index.html - This file contains the HTML code for the quiz application, including the home screen, quiz screen, and results screen.
+- style.css - This file contains the CSS code for styling the quiz application.
+- script.js - This file contains the JavaScript code for running the quiz application, including the countdown timer and the quiz questions.
+
+##Dependencies
+The quiz application does not have any external dependencies. It only uses HTML, CSS, and JavaScript.
+
+## Credits
+The quiz application was built by Camile Sullins.
+
+## Demo
+
+![Code Quiz Demo](./assets/images/Code%20Quiz!.gif)
--- assets/js/script.js
+++ assets/js/script.js
@@ -3,14 +3,17 @@ let header = document.querySelector('#header');
 let cards = document.querySelector('#cards');
 let seconds = document.querySelector('.seconds');
 const clickStart = $('.start');
-const quizStart = $('.header');
 let choicesElement = document.querySelector('#choices');
 let questionElement = document.querySelector('#question');
 let currentQuestionIndex = 0;
 let currentScore = 0;
-let timeLeft = 25;
+let timeLeft = 30;
 let timerRunning = false;
+let form = document.querySelector('form');
+let user = document.querySelector('.user');
+let highScore = document.querySelector('.highScore');
 
+//On initial load, only the header is visible on the page. When user clicks on START, this function hides the header and displays the cards with questions and answers. Then starts the timer function, and begins displaying the questions. 
 clickStart.on('click', function() {
     header.style.display = 'none';
     cards.style.display = 'flex';
@@ -46,60 +49,60 @@ function startTimer() {
       }
     }, 1000);
   };
-
+// The six multiple choice questions are below.
 const questions = [
       {
-          question: 'What is the capital of France?',
-          answers: ['London', 'Paris', 'Madrid', 'Berlin'],
-          correctAnswer: 1 // Index of the correct answer in the answers array
+          question: 'How do you declare a variable in JavaScript?',
+          answers: ['Using the keyword "const"', 'Using the keyword "var"', 'Using the keyword "let"', 'All of the above'],
+          correctAnswer: 3 // Index of the correct answer in the answers array
         },
         {
-            question: 'What is the largest mammal in the world?',
-            answers: ['Elephant', 'Giraffe', 'Blue whale', 'Rhinoceros'],
+            question: 'What is the purpose of the <head> tag in HTML?',
+            answers: ['To define the main content of the page', 'To define the navigation links on the page', 'To define the footer of the page', 'To contain the metadata of the page, such as the title and description'],
+            correctAnswer: 3
+        },
+        {
+            question: 'What is an array in JavaScript?',
+            answers: ['A type of function', 'A value that can be changed or assigned a new value', 'A collection of data', 'A type of loop'],
             correctAnswer: 2
         },
         {
-            question: 'who is the cutest labradoodle in the world?',
-            answers: ['Ben', 'Remi', 'Archie', 'Cami'],
-            correctAnswer: 1
+            question: 'What is the CSS syntax to change text color to blue?',
+            answers: ['text-color: blue', 'fontcolor=blue', 'font-color: blue', 'color: blue'],
+            correctAnswer: 3
         },
         {
-            question: 'Who is the best husband in the world?',
-            answers: ['Archie', 'Remi', 'Ben', 'Cami'],
-            correctAnswer: 2
+            question: 'What is the purpose of the <img> tag in HTML?',
+            answers: ['To display an image on the page', 'To add a hyperlink to the page', 'To display a video on the page', 'To add a button to the page'],
+            correctAnswer: 0
         },
     ];
-    
-
+    // This code qill begin displaying questions, one at a time from the questions object
 function displayQuestion(question) {
     console.log('Questions are displaying.');
-
+//when called, a newQuestion variable will serve to create the p tag where the question will display from the questions object
     let newQuestion = document.createElement('p');
     newQuestion.textContent = question.question;
     questionElement.appendChild(newQuestion);
-
+//this code uses the 'forEach' method to loop through the array of answers on each object item (question) and create a div, 
     let answersElement = document.createElement('div');
     question.answers.forEach((answer, index) => {
     const answerButton = document.createElement('button');
-
+//then creates a button with each answer choice in the textcontent
     answerButton.textContent = answer;
-
+//when an answer choice is clicked on
     answerButton.addEventListener('click', () => {
-        
+        //if it matches the correctAnswer (dicated from the questions object property...)
         if (index === question.correctAnswer) {
-            // Display next question if answer is correct
+            // then add to currentScore if answer is correct
             currentScore++
-        
+        //if the user chose an incorrect answer, 5 seconds subtracts from the timer
         } else {
-            // Incorrect answer, display message
-            const messageElement = document.createElement('p');
-            messageElement.textContent = 'Incorrect';
-            choicesElement.appendChild(messageElement);
             timeLeft -= 5;
         }
-
+//display the next question
         currentQuestionIndex++;
-
+// as long as the user still has questions to loop through, continue displaying the next question via the displayQuestion func.
         if (currentQuestionIndex < questions.length) {
             const nextQuestion = questions[currentQuestionIndex];
             questionElement.innerHTML = '';
@@ -107,25 +110,56 @@ function displayQuestion(question) {
         } else {
             displayResults();
         }
-
     });
         answersElement.appendChild(answerButton);
     });
+   // this overwrites the answer choices each time to correspond with the question 
     choicesElement.innerHTML = '';
     choicesElement.appendChild(answersElement);
     }
-    
-
 
     function displayResults() {
-        timerRunning = false;
+        // clear out the question and answer sections and 
         questionElement.innerHTML = '';
         choicesElement.innerHTML = '';
         let score = currentScore;
-        const resultsElement = document.querySelector('.card2');
-        resultsElement.innerHTML = `You answered ${score} out of ${questions.length} correctly.`;
+// in the results section, card1, display the phrase below with the user's score
+        const resultsElement = document.querySelector('.card1');
+        $('.card1').css({'color':'rgb(255, 242, 0)' ,'font-size': '13px'});
+        resultsElement.innerHTML = `You answered ${score} out of ${questions.length} correctly!`;
+        //highScore.innerHTML = timeLeft;
+        form.style.display = 'flex';
 
-        let highScore = document.querySelector('.highScore');
-
-        highScore.innerHTML = timeLeft;
     };
+    loadLastScore();
+
+    function submitForm(event) {
+
+        let initials = document.querySelector('#initials').value;
+        let score = localStorage.getItem("highScore");
+        
+        if (!score) {
+            score = 0;
+        }
+        
+        if (score > (timeLeft + 1))
+        return;
+        
+        localStorage.setItem("initials", initials);
+        localStorage.setItem("highScore", (timeLeft + 1));
+
+      console.log(timeLeft);
+
+    }
+// This store the initials and score...
+    function loadLastScore() {
+       let initials = localStorage.getItem("initials");
+       let score = localStorage.getItem("highScore");
+    //only if it is not present
+       if (!initials || !score) {
+          return;
+        }
+// display the initials and score in the nav sections with classes 'user' and 'highScore'
+        user.innerHTML = initials;
+        highScore.innerHTML = score;
+      }
